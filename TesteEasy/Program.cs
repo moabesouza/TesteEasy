3f227b86268b2f0b07110d27ca3f4eebed03632d
moabesouza <mmoabesouza6@gmail.com>
2024-09-03 09:06:54 -0300

    Melhoria no método CheckSubarraySum

--- TesteEasy/Program.cs
+++ TesteEasy/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public class Solution
 {
@@ -9,32 +10,48 @@ public class Solution
     /// <param name="nums">Array de inteiros a ser analisado.</param>
     /// <param name="k">Número inteiro usado para verificar se a soma de uma subarray é múltiplo.</param>
     /// <returns>Retorna `true` se existir uma subarray cuja soma seja múltiplo de `k`, caso contrário `false`.</returns>
-    public bool CheckSubarraySum(int[] nums, int k)
-    {     
-        for (int i = 0; i < nums.Length - 1; i++)
+    public bool SubarraySum(int[] nums, int k)
+    {
+       
+        var restos = new Dictionary<int, int>();
+        restos[0] = -1;
+
+        int somaPrefixada = 0;
+
+        for (int i = 0; i < nums.Length; i++)
         {
-            int sum = nums[i];
-         
-            for (int j = i + 1; j < nums.Length; j++)
+            somaPrefixada += nums[i];
+
+            int resto = somaPrefixada % k;
+            if (resto < 0) resto += k;
+
+            if (restos.ContainsKey(resto))
             {
-                sum += nums[j];       
-                if (sum % k == 0)                
+                if (i - restos[resto] > 1) 
+                {
                     return true;
-                
+                }
             }
-        }        
+            else
+            {
+                restos[resto] = i;
+            }
+        }
+
         return false;
     }
 
-
+  
     public static void Main(string[] args)
     {
         var solution = new Solution();
-   
+
+      
         int[] nums = { 23, 2, 6, 4, 7 };
         int k = 6;
-        bool result = solution.CheckSubarraySum(nums, k);
+        bool result = solution.SubarraySum(nums, k);
 
+ 
         Console.WriteLine($"Resultado: {result}");
     }
 }
